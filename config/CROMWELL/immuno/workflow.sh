b7d2b51eec887309a87086f896329c9840e6f6e8
John Major <daylily@daylilyinformatics.com>
2024-08-25 08:03:06 -0700

    X

--- config/CROMWELL/immuno/workflow.sh
+++ config/CROMWELL/immuno/workflow.sh
@@ -11,6 +11,8 @@ cd $workflow_dir
 
 
 
+# Germine WGS
+# java -Dbackend.default=local -Dconfig.file=config/CROMWELL/app.conf  -jar /usr/local/bin/cromwell.jar run config/CROMWELL/immuno/analysis-wdls/definitions/germline_wgs.wdl -i config/CROMWELL/immuno/jem_germline_wgs_in.json
 
-
+# IMMUNO
 #java -Dbackend.default=local -Dconfig.file=config/CROMWELL/app.conf  -jar /usr/local/bin/cromwell.jar run config/CROMWELL/immuno/analysis-wdls/definitions/immuno.wdl -i config/CROMWELL/immuno/example_immuno_cloud-WDL.json
