1c4950141136d17527287008bf91327a8642b838
John Major <daylily@daylilyinformatics.com>
2024-08-25 07:55:39 -0700

    X

--- config/CROMWELL/immuno/workflow.sh
+++ config/CROMWELL/immuno/workflow.sh
@@ -7,8 +7,10 @@ workflow_dir=$PWD
 cd $workflow_dir
 
 
-java -jar /usr/local/bin/womtool.jar validate config/CROMWELL/immuno/analysis-wdls/definitions/immuno.wdl
+# java -jar /usr/local/bin/womtool.jar validate config/CROMWELL/immuno/analysis-wdls/definitions/immuno.wdl
 
-#java -Dbackend.default=Local -Dconfig.file=config/CROMWELL/app.conf  -jar /usr/local/bin/cromwell.jar run config/CROMWELL/immuno/analysis-wdls/definitions/immuno.wdl -i config/CROMWELL/immuno/example_immuno_cloud-WDL.yaml
 
-#java -Dbackend.default=Local -Dconfig.file=config/MGI_example/app.conf -jar /usr/local/bin/cromwell.jar run config/MGI_example/example.wdl -i config/MGI_example/inputs.json
+
+
+
+#java -Dbackend.default=local -Dconfig.file=config/CROMWELL/app.conf  -jar /usr/local/bin/cromwell.jar run config/CROMWELL/immuno/analysis-wdls/definitions/immuno.wdl -i config/CROMWELL/immuno/example_immuno_cloud-WDL.json
