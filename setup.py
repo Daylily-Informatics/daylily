3e87de025535d195ed232d1d32c0cb4889b49b5f
John Major <daylily@daylilyinformatics.com>
2024-08-25 00:52:56 -0700

    modified align rules to use igz and remove unecessary samtools view

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily",
-    version="0.6.3",
+    version="0.6.9",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here,
