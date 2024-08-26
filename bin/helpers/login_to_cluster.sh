094c2e163da5c92a028df19b4c747c2fa490a043
John Major <daylily@daylilyinformatics.com>
2024-08-26 15:37:36 -0700

    X

--- bin/helpers/login_to_cluster.sh
+++ bin/helpers/login_to_cluster.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-pcluster describe-cluster -n $cluster_name | grep 'publicIpAddress' | perl -pe 's/.*\: \"(.*)\"\,.*/$1/g;' &&  cluster_ip=$(pcluster describe-cluster -n $cluster_name | grep 'publicIpAddress' | perl -pe 's/.*\: \"(.*)\"\,.*/$1/g;') && ssh -i ~/.ssh/daylily2.pem ubuntu@$cluster_ip
+#source me
+conda activate DAYCLI && cluster_name=$(conda activate DAYCLI && pcluster list-clusters | grep 'clusterName' | perl -pe 's/.*\: \"(.*)\"\,.*/$1/g;') && cluster_ip=$(pcluster describe-cluster -n $cluster_name | grep 'publicIpAddress' | perl -pe 's/.*\: \"(.*)\"\,.*/$1/g;') &&ssh -i ~/.ssh/daylily2.pem ubuntu@$cluster_ip
