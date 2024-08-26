505a29fc18b38cabce2499eeb11a766eb09fc3ca
John Major <daylily@daylilyinformatics.com>
2024-08-26 15:00:19 -0700

    new cluster setup config running nicely, all local tests completing.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily",
-    version="0.6.10",
+    version="0.6.11",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here,
