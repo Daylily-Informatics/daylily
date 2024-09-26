6832c45d1cc198d5c23c1ce956285897ea3d2710
John Major <daylily@daylilyinformatics.com>
2024-09-26 00:20:31 -0700

    closer to fin

--- bin/helpers/watch_cluster_status.py
+++ bin/helpers/watch_cluster_status.py
@@ -1,11 +1,11 @@
 import sys
 import os
 
-
+region = sys.argv[1]
 # Continuously check the output
 dot = "."
 while True:
-    ret_val = os.popen("""pcluster list-clusters | grep clusterStatus | cut -d '"' -f 4""").readline().rstrip()
+    ret_val = os.popen(f"""pcluster list-clusters --region {region} | grep clusterStatus | cut -d '"' -f 4""").readline().rstrip()
     # Read a line of output from the process
     
     if ret_val not in ['CREATE_IN_PROGRESS']:
