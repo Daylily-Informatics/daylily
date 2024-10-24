934f3c2275c5017bd721bb57a2f3b604a3c11078
John Major <daylily@daylilyinformatics.com>
2024-10-24 02:50:05 -0700

    X

--- bin/create_daylily_omics_analysis_s3.sh
+++ bin/create_daylily_omics_analysis_s3.sh
@@ -9,7 +9,7 @@ dryrun=false  # Default to false
 
 # Usage function
 usage() {
-    echo "Usage: $0 [--bucket-prefix <prefix> --daylily-s3-version <version> (default v0.7)] [--region <region> (default us-west-2)] [--dryrun] [--help]"
+    echo "Usage: $0 [--bucket-prefix <prefix> --daylily-s3-version <version> (default v0.7)] [--region <region> (default us-west-2)] [--dryrun] [--help] --disable-warn"
     exit 1
 }
 
@@ -21,10 +21,24 @@ while [[ "$#" -gt 0 ]]; do
     --region) region="$2"; shift 2;;
     --dryrun) dryrun=true; shift 1;;
     --bucket-prefix) bucket_prefix="$2"; shift 2;;
+    --disable-warn) disable_warn=true; shift 1;;
     --help) usage;;
     *) echo "Unknown parameter: $1"; usage;;
   esac
 done
+
+if [ "$disable_warn" != true ]; then
+    echo "Warning: This script will create a new S3 bucket and copy data from the Daylily reference data bucket."
+    echo "The new bucket will be created with the prefix specified by the --bucket-prefix argument."
+    echo "The script will copy the following data from the Daylily reference data bucket:"
+    echo "  - cluster_boot_config"
+    echo "  - data"
+    echo "The script will run the following commands in parallel:"
+    echo "  - aws s3 cp s3://daylily-references-public/cluster_boot_config s3://<new-bucket>/cluster_boot_config --recursive --request-payer requester"
+    echo "  - aws s3 cp s3://daylily-references-public/data s3://<new-bucket>/data --recursive --request-payer requester"
+    echo ""
+    echo "You are advised to enable accelerated transfer for the new bucket to speed up data transfer."    
+fi
 
 if [[ "$s3_reference_data_version" != "v0.7" ]]; then
     echo "Error: Only version 'v0.7' is supported. Exiting."
@@ -33,8 +47,6 @@ else
     echo "Using Daylily S3 reference data version: $s3_reference_data_version"
     source_bucket="daylily-references-public"
 fi
-
-
 
 if [[ -z "$bucket_prefix" ]]; then
     echo "Error: Bucket prefix is required."
@@ -83,8 +95,8 @@ fi
 
 # Compose bucket creation commands
 
-cmda="aws s3 cp s3://daylily-references-public/cluster_boot_config/${s3_reference_data_version} s3://${new_bucket}/cluster_boot_config --recursive  --request-payer requester"
-cmdb="aws s3 cp s3://daylily-references-public/data/${s3_reference_data_version} s3://${new_bucket}/data --recursive --request-payer requester"
+cmda="aws s3 cp s3://${source_bucket}/cluster_boot_config s3://${new_bucket}/cluster_boot_config --recursive  --request-payer requester"
+cmdb="aws s3 cp s3://${source_bucket}/data s3://${new_bucket}/data --recursive --request-payer requester"
 
 # Execute the commands in parallel
 echo "Creating buckets and copying data..."
