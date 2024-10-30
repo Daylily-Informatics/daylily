e46562f8949594d2d6239ff13082e8be0987f63c
John Major <daylily@daylilyinformatics.com>
2024-10-29 17:48:39 -0700

    0.7.30

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily",
-    version="0.7.29",
+    version="0.7.30",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here,
