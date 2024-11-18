1922d0c0a0d42e8a078a4d51073f805659415f96
AWS ParallelCluster user <ubuntu@ip-10-0-0-196.us-west-2.compute.internal>
2024-11-18 11:39:42 +0000

    0.7.89

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Daylily AWS Ephemeral Cluster Setup (0.7.88)
+# Daylily AWS Ephemeral Cluster Setup (0.7.89)
 
 **beta release**
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily",
-    version="0.7.88",
+    version="0.7.89",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here,
