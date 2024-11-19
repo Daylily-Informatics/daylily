c7a0653d406887cd7a5a6a5caf6d37f8469f3a45
AWS ParallelCluster user <ubuntu@ip-10-0-0-196.us-west-2.compute.internal>
2024-11-19 05:47:04 +0000

    0.7.90

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily",
-    version="0.7.90",
+    version="0.7.91",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here,
