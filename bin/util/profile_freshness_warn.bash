075aa73a18a1edce7b789de7da73d7accfbda857
John Major <daylily@daylilyinformatics.com>
2024-11-27 00:46:38 -0800

    X

--- bin/util/profile_freshness_warn.bash
+++ bin/util/profile_freshness_warn.bash
@@ -2,43 +2,56 @@
 
 profile_dir=$1
 
-echo " "
-ctr="0"
-for i in {1..54}; do
 
-    r=$(( 201 + $i ))
-    g=$(( (( $i * 4))+39 ))
-    b=$(( 54- $i   ))
+if [[ ! -f "config/day_profiles/$DAY_PROFILE/rule_config_lowcov.yaml" && ! -f "config/day_profile/$DAY_PROFILE/cluster.yaml"  && ! -f "config/day_profiles/$DAY_PROFILE/rule_config.yaml" ]]; then
+    sleep 0.1
+elif [[  "config/day_profiles/$DAY_PROFILE/templates/rule_config_lowcov.yaml" -nt  "config/day_profiles/$DAY_PROFILE/rule_config_lowcov.yaml" || "config/day_profiles/$DAY_PROFILE/templates/cluster.yaml" -nt "config/day_profiles/$DAY_PROFILE/cluster.yaml" || "config/day_profiles/$DAY_PROFILE/templates/config.yaml" -nt "config/day_profiles/$DAY_PROFILE/config.yaml" || "config/day_profiles/$DAY_PROFILE/templates/rule_config.yaml" -nt "config/day_profiles/$DAY_PROFILE/rule_config.yaml" || "config/day_profiles/$DAY_PROFILE/templates/profile.info" -nt "config/day_profiles/$DAY_PROFILE/config.yaml" ]]; then
+    source bin/util/profile_freshness_warn.bash $profile_dir
 
-    rr=$(( 201 + $i ))
-        gg=$(( 0 ))
-        bb=$(( 147 + $i*2))
 
-    c=$(echo """printf %"$(( $i + 1 ))"s""")
-    x=$($c)
-    rs=$(echo "$x" | sed 's/ /\\/g')
-    if [[ "$ctr" == "2" ]]; then
-            colr   "$rs" "$r, $g, $b" "$rr, $gg, $bb" "2"
+    echo " "
+    ctr="0"
+    for i in {1..54}; do
 
-    fi;
-    ctr=$(( $ctr +1 ))
-    if [[ "$ctr" == "3" ]]; then
-        ctr="1"
-    fi
-done;
+        r=$(( 201 + $i ))
+        g=$(( (( $i * 4))+39 ))
+        b=$(( 54- $i   ))
 
-colr -n "  =================||>" "$DY_WT0" "$DY_WB0" "$DY_ES1" && colr -n "  WARNING  " "$DY_ET0" "black" "$DY_ES0" && colr "<||=================  " "$DY_WT0" "$DY_WB0" "$DY_ES1"
-colr "  One+ of the template config files in :               " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-colr "$profile_dir/templates" "$DY_IT0" "$DY_IB1" "$DY_IS2"
-colr "     is newer than your active config files.           " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-colr "  You hace 2 options. 1) remove the active files  :    " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-colr "rm $profile_dir/*  " "$DY_IT0" "$DY_IB1" "$DY_IS2"
+        rr=$(( 201 + $i ))
+            gg=$(( 0 ))
+            bb=$(( 147 + $i*2))
 
-colr "    so the new template files can be copied there. Or  " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-colr "    2) Keep the active local copies.  You may touch    " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-colr "    them to avoid this error:                          " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-colr "touch $profile_dir/*.yaml " "$DY_IT0" "$DY_IB1" "$DY_IS2"
-colr "     will keep the files and avoid this warning/block. " "$DY_ET2" "$DY_EB1" "$DY_ES2"
-echo " "
-echo " "
+        c=$(echo """printf %"$(( $i + 1 ))"s""")
+        x=$($c)
+        rs=$(echo "$x" | sed 's/ /\\/g')
+        if [[ "$ctr" == "2" ]]; then
+                colr   "$rs" "$r, $g, $b" "$rr, $gg, $bb" "2"
 
+        fi;
+        ctr=$(( $ctr +1 ))
+        if [[ "$ctr" == "3" ]]; then
+            ctr="1"
+        fi
+    done;
+
+    colr -n "  =================||>" "$DY_WT0" "$DY_WB0" "$DY_ES1" && colr -n "  WARNING  " "$DY_ET0" "black" "$DY_ES0" && colr "<||=================  " "$DY_WT0" "$DY_WB0" "$DY_ES1"
+    colr "  One+ of the template config files in :               " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    colr "$profile_dir/templates" "$DY_IT0" "$DY_IB1" "$DY_IS2"
+    colr "     is newer than your active config files.           " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    colr "  You hace 2 options. 1) remove the active files  :    " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    colr "rm $profile_dir/*  " "$DY_IT0" "$DY_IB1" "$DY_IS2"
+
+    colr "    so the new template files can be copied there. Or  " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    colr "    2) Keep the active local copies.  You may touch    " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    colr "    them to avoid this error:                          " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    colr "touch $profile_dir/*.yaml " "$DY_IT0" "$DY_IB1" "$DY_IS2"
+    colr "     will keep the files and avoid this warning/block. " "$DY_ET2" "$DY_EB1" "$DY_ES2"
+    echo " "
+    echo " "
+
+
+
+else
+    colr "Your config files in $profile_dir are newer than the templates. clear 2 go." "$DY_WT1" "$DY_WB2" "$DY_WS1 " 1>2
+    sleep 0.1
+fi;
