823af301396106d11acd37d939b86f541b5001e9
John Major <daylily@daylilyinformatics.com>
2024-11-27 01:03:40 -0800

    X

--- bin/util/profile_freshness_warn.bash
+++ bin/util/profile_freshness_warn.bash
@@ -1,10 +1,38 @@
 
 
-profile_dir=$1
+profile_dir=$DAY_PROFILE_DIR
 
 
-if [[ ! -f "config/day_profiles/$DAY_PROFILE/rule_config_lowcov.yaml" && ! -f "config/day_profile/$DAY_PROFILE/cluster.yaml"  && ! -f "config/day_profiles/$DAY_PROFILE/rule_config.yaml" ]]; then
-    sleep 0.1
+if [[ ! -f "$DAY_PROFILE_DIR/rule_config.yaml" && ! -f "$DAY_PROFILE_DIR/config.yaml"  && ! -f "$DAY_PROFILE_DIR/profile_env.sh" ]]; then
+    
+
+    colr -n " > >> >>> " "$DY_IB0" "$DY_IT0" "$DY_IS0" 1>&2
+    colr "COPYING TEMPLATE FILES TO ACTIVE CONFIG FILES IN $profile_dir - none detected" "$DY_IT0" "$DY_IB0" "$DY_IS1" 1>&2
+
+    profile_files=("$profile_dir/rule_config_lowcov.yaml" "$profile_dir/cluster.yaml" "$profile_dir/rule_config.yaml")
+    templates=("$profile_dir/templates/rule_config_lowcov.yaml" "$profile_dir/templates/cluster.yaml" "$profile_dir/templates/rule_config.yaml")
+
+    for i in "${!profile_files[@]}"; do
+        if [[ ! -f "${profile_files[i]}" || "${templates[i]}" -nt "${profile_files[i]}" ]]; then
+            echo "Copying template files to active config files in $profile_dir"
+            cp "$profile_dir/templates/"*yaml "$profile_dir/" || {
+                echo "ERROR: Failed to copy template files."
+                exit 48
+            }
+            break
+        fi
+    done
+
+    # Replace placeholders in config files
+    re_safe_mgr=$(echo "$DAY_ROOT" | sed "s/\//\\\\\//g")
+    sed -i "s/.DAY_ROOT\/c/$re_safe_mgr\/c/g" "$profile_dir/"*yaml
+    bn=$(basename "$profile_dir" | sed 's/-/\\-/g')
+    sed -i "s/BASENAME_REGSUB/$bn/g" "$profile_dir/"*yaml
+    u="$USER"
+    sed -i "s/USER_REGSUB/$u/g" "$profile_dir/"*yaml
+    hn="$HOSTNAME"
+    sed -i "s/HOSTNAME/$hn/g" "$profile_dir/"*yaml
+
 elif [[  "config/day_profiles/$DAY_PROFILE/templates/rule_config_lowcov.yaml" -nt  "config/day_profiles/$DAY_PROFILE/rule_config_lowcov.yaml" || "config/day_profiles/$DAY_PROFILE/templates/cluster.yaml" -nt "config/day_profiles/$DAY_PROFILE/cluster.yaml" || "config/day_profiles/$DAY_PROFILE/templates/config.yaml" -nt "config/day_profiles/$DAY_PROFILE/config.yaml" || "config/day_profiles/$DAY_PROFILE/templates/rule_config.yaml" -nt "config/day_profiles/$DAY_PROFILE/rule_config.yaml" || "config/day_profiles/$DAY_PROFILE/templates/profile.info" -nt "config/day_profiles/$DAY_PROFILE/config.yaml" ]]; then
     source bin/util/profile_freshness_warn.bash $profile_dir
 
