638d8b7f02fce4ce6f6794716dc67c7aa1ad2dbc
John Major <daylily@daylilyinformatics.com>
2025-02-19 09:27:19 -0800

    0.7.162

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Daylily AWS Ephemeral Cluster Setup
-_(0.7.161)_
+_(0.7.162)_
 
 **beta release**
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="daylily",
-    version="0.7.161",
+    version="0.7.162",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here
