a36214ed83c89b0aca58dce580875159d3f5f83a
Ubuntu <ubuntu@ip-172-31-37-81.us-west-2.compute.internal>
2025-03-11 21:56:32 +0000

    X

--- bin/merge_caller_vcfs.bash
+++ bin/merge_caller_vcfs.bash
@@ -3,46 +3,68 @@
 set -euo pipefail
 
 # Define input files
-SAMPLE="RIH0_ANA0-HG002-19_DBC0_0.bwa2a"
-CALLERS=("clair3" "deep" "lfq2" "oct" "sentd")
-CALLER_NAMES=("clair3" "deepvariant" "lofreq2" "octopus" "sentieon")
+SAMPLE="RIH0_ANA0-HG002-19_DBC0_0"
+CALLERS=("clair3" "deep" "lfq2" "sentd")
+CALLER_NAMES=("clair3" "deepvariant" "lofreq2" "sentieon")
 
 # Annotate each caller's VCF
 for i in "${!CALLERS[@]}"; do
-  CALLER_FILE="${SAMPLE}.${CALLERS[$i]}.snv.sort.vcf.gz"
+  CALLER=${CALLERS[$i]}
+  CALLER_FILE="${SAMPLE}.bwa2a.${CALLERS[$i]}.snv.sort.rh.vcf.gz"
   CALLER_LABEL="${CALLER_NAMES[$i]}"
   OUTPUT_FILE="${CALLER_LABEL}.annotated.vcf.gz"
 
   echo "Annotating $CALLER_FILE with caller: $CALLER_LABEL"
-  bcftools annotate --threads 4 -Oz \
-    --set-id '+' \
-    --annotations "INFO/caller=${CALLER_LABEL}" \
-    "$CALLER_FILE" \
-    > "$OUTPUT_FILE"
 
-  bcftools index "$OUTPUT_FILE"
+  echo "A" $CALLER .. $CALLER_FILE
+  bcftools annotate \
+    -x INFO/AF,FORMAT/AF \
+    -h <(echo '##INFO=<ID=caller,Number=.,Type=String,Description="Source caller">') \
+    --set-id +'%CHROM:%POS:%REF:%ALT' \
+    --set "INFO/caller=$CALLER" \
+     -Oz \
+    -o ${OUTPUT_FILE} $CALLER_FILE
+
+  tabix $OUTPUT_FILE
+  bcftools index $OUTPUT_FILE
+
+
 done
 
 # Merge annotated VCFs into one
-MERGED_VCF="${SAMPLE}.merged_callers.vcf.gz"
+CONCAT_VCF="${SAMPLE}.concat_callers.vcf.gz"
 
-bcftools merge --threads 8 -Oz \
+bcftools concat -a --threads 8 -Oz \
+  --merge all --force-samples \
   --info-rules caller:join \
   clair3.annotated.vcf.gz \
   deepvariant.annotated.vcf.gz \
   lofreq2.annotated.vcf.gz \
-  octopus.annotated.vcf.gz \
   sentieon.annotated.vcf.gz \
-  > "$MERGED_VCF"
+  > "$CONCAT_VCF"
 
 # Sort and index merged VCF
+tabix $CONCAT_VCF
+bcftools index $CONCAT_VCF
+
 SORTED_VCF="${SAMPLE}.merged_callers.sorted.vcf.gz"
-bcftools sort -Oz -o "$SORTED_VCF" "$MERGED_VCF"
-bcftools index "$SORTED_VCF"
+bcftools sort -Oz -o "$SORTED_VCF" $CONCAT_VCF
+
+bcftools norm -Oz -m+any \
+  $SORTED_VCF \
+  -o all_callers.merged.vcf.gz
+
+tabix all_callers.merged.vcf.gz
+bcftools index all_callers.merged.vcf.gz
+
+bcftools sort -Oz -o all_callers.merged.sort.vcf.gz all_callers.merged.vcf.gz
+
+bcftools index all_callers.merged.sort.vcf.gz
+tabix all_callers.merged.sort.vcf.gz
 
 # Optional stats for QC
 echo "Generating stats for merged VCF"
-bcftools stats "$SORTED_VCF" > "${SORTED_VCF%.vcf.gz}.stats.txt"
+#bcftools stats "$SORTED_VCF" > "${SORTED_VCF%.vcf.gz}.stats.txt"
 
 echo "Merged VCF created successfully: $SORTED_VCF"
 
