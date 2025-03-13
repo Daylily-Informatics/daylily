b387c44aad2ddd20d59a9e10f2fe1511a6deb251
John Major <daylily@daylilyinformatics.com>
2025-03-13 03:31:04 -0700

    merge upd to incl aligner

--- bin/merge_caller_vcfs.bash
+++ bin/merge_caller_vcfs.bash
@@ -3,33 +3,38 @@
 set -euo pipefail
 
 # Define input files
+ALIGNERS=("bwa2a" "strobe")
 SAMPLE="RIH0_ANA0-HG002-19_DBC0_0"
 CALLERS=("clair3" "deep" "lfq2" "sentd" "oct")
 CALLER_NAMES=("clair3" "deepvariant" "lofreq2" "sentieon" "octopus")
 
 # Annotate each caller's VCF
-for i in "${!CALLERS[@]}"; do
-  CALLER=${CALLERS[$i]}
-  CALLER_FILE="${SAMPLE}.bwa2a.${CALLERS[$i]}.snv.sort.rh.vcf.gz"
-  CALLER_LABEL="${CALLER_NAMES[$i]}"
-  OUTPUT_FILE="${CALLER_LABEL}.annotated.vcf.gz"
 
-  echo "Annotating $CALLER_FILE with caller: $CALLER_LABEL"
+for ai in ALIGNERS; do
 
-  #    -x INFO/AF,FORMAT/AF \
+  for i in "${!CALLERS[@]}"; do
+    CALLER=${CALLERS[$i]}
+    ALIGNER=${ALIGNERS[$ai]}
+    CALLER_FILE="${SAMPLE}.${ALIGNER}.${CALLERS[$i]}.snv.sort.rh.vcf.gz"
+    CALLER_LABEL="${CALLER_NAMES[$i]}"
+    OUTPUT_FILE="${CALLER_LABEL}.annotated.vcf.gz"
 
-  echo "A" $CALLER .. $CALLER_FILE
-  bcftools annotate \
-    -h <(echo '##INFO=<ID=caller,Number=.,Type=String,Description="Source caller">') \
-    --set-id +'%CHROM:%POS:%REF:%ALT' \
-    --set "INFO/caller=$CALLER" \
-     -Oz \
-    -o ${OUTPUT_FILE} $CALLER_FILE
+    echo "Annotating $CALLER_FILE with caller: $CALLER_LABEL"
 
-  tabix $OUTPUT_FILE
-  bcftools index $OUTPUT_FILE
+    #    -x INFO/AF,FORMAT/AF \
 
+    echo "A" $CALLER .. $CALLER_FILE
+    bcftools annotate \
+      -h <(echo '##INFO=<ID=aligner_caller,Number=.,Type=String,Description="Source aligner-caller">') \
+      --set-id +'%CHROM:%POS:%REF:%ALT' \
+      --set "INFO/aligner_caller=$ALIGNER\-$CALLER" \
+      -Oz \
+      -o ${OUTPUT_FILE} $CALLER_FILE
 
+    tabix $OUTPUT_FILE
+    bcftools index $OUTPUT_FILE
+
+  done
 done
 
 # Merge annotated VCFs into one
